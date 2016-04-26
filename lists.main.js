a2141ee1453782bf4b69e54ec750076518d4994f
arnoldllanes <arn0ldllanes@yahoo.com>
2016-04-25 23:26:42 -0700

    Left off @ lists 5:10

---
+++ lists.main.js
@@ -0,0 +1,11 @@
+new Vue({
+	el: '#app',
+
+	data: {
+		tasks: [
+			{ body: 'Go to the store', completed: false},
+			{ body:'Go to the bank', completed: false},
+			{ body:'Go to the doctor', completed: true}
+		]
+	}
+});
