342b67a026ab3fc3fdaae67100fbb4924e43ab87
arnoldllanes <arn0ldllanes@yahoo.com>
2016-04-26 18:04:08 -0700

    9/27

--- lists.main.js
+++ lists.main.js
@@ -1,3 +1,28 @@
+Vue.component('tasks', {
+	template: '#tasks-template',
+	
+	props: ['list'],
+	//computed property for {{ remaining }} to show how many items are in progress
+	computed: {
+		remaining: function(){
+			return this.list.filter(this.inProgress).length;
+		}
+	},
+	methods: {
+		isCompleted: function(task){
+			return task.completed;
+		},
+		inProgress: function(task){
+			return ! this.isCompleted(task);
+		},
+		deleteTask: function(task){
+			this.list.$remove(task);
+		},
+		clearCompleted: function(task){
+			this.list = this.list.filter(this.inProgress);
+		}
+	}
+});
 new Vue({
 	el: '#app',
 
